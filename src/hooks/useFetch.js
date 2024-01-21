834ac129ce27cba7c7c66c86f361ae6f0ef6a897
Diman1988 <diman7988@gmail.com>
2024-01-21 15:55:18 +0100

    feat: custom hook useFetch added

---
+++ src/hooks/useFetch.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from "react";
+import { FakeFetch } from "../services/api";
+
+export function useFetch({ url }) {
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const fakeFetch = new FakeFetch();
+        const response = await fakeFetch.fetch(url);
+
+        if (response.status === 200) {
+          setCustomers(response.data);
+        }
+      } catch (error) {
+        console.log("Request to API failed:", error);
+        setCustomers([]);
+      }
+    };
+
+    fetchData();
+  }, [url]);
+
+  return customers;
+}
