089c1c26c0e35f8d5e989594cd889c4bd9c2c307
Diman1988 <diman7988@gmail.com>
2024-01-21 15:51:55 +0100

    feat: fakeFetchApi class

---
+++ src/services/api.js
@@ -0,0 +1,83 @@
+import { customersListGenerator } from "../helper/customersListGenerator";
+import { transactionsListGenerator } from "../helper/transactionsListGenerator";
+
+const TRANSACTIONS = "transactions";
+const CUSTOMERS = "customers";
+
+/**
+ * FakeFetch is a class that simulates the fetch API fo some urls.
+ *
+ */
+class FakeFetch {
+  static instance;
+  customers;
+  transactions;
+
+  constructor() {
+    if (FakeFetch.instance) return FakeFetch.instance;
+
+    this.init();
+
+    FakeFetch.instance = this;
+  }
+  init() {
+    this.customers = customersListGenerator(100);
+    this.transactions = transactionsListGenerator(this.customers, 100);
+  }
+  fetch(url) {
+    if (!url) return this.#response(undefined);
+
+    const { resource, id } = this.#parseUrl(url);
+    const data = this.#processRequest(resource, id);
+
+    return this.#response(data);
+  }
+  #parseUrl(url) {
+    if (url === undefined) return undefined;
+
+    const urlParts = url.split("/");
+    const resource = urlParts[1];
+    const id = urlParts[2];
+
+    return { resource, id };
+  }
+  #processRequest(resource, id) {
+    switch (resource) {
+      case CUSTOMERS:
+        if (id) {
+          return this.customers.find((customer) => customer.id === id);
+        } else {
+          return this.customers;
+        }
+      case TRANSACTIONS:
+        if (id) {
+          return this.transactions.find((transaction) => transaction.id === id);
+        } else {
+          return this.transactions;
+        }
+      default:
+        return undefined;
+    }
+  }
+  #response(data) {
+    return new Promise((resolve, reject) => {
+      if (data === undefined) {
+        const response = {
+          status: 404,
+          statusText: "Not Found",
+        };
+        reject(response);
+      }
+      setTimeout(() => {
+        const response = {
+          status: 200,
+          statusText: "Endpoint Found",
+          data,
+        };
+        resolve(response);
+      }, 1000);
+    });
+  }
+}
+
+export { FakeFetch };
